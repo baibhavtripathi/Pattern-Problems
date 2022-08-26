91b4a3ca7ce5c4a94e6a165a6237b0b89fff6f9a
baibhavtripathi <vaibhavtripathi1629@gmail.com>
2022-08-26 09:23:21 +0530

    Added Average

--- Python Patterns/Sliding Window/AverageOfN.py
+++ Python Patterns/Sliding Window/AverageOfN.py
@@ -1,3 +1,12 @@
-from pip import main
-
-
+# Time Complexity : O(N)
+# Space Compleity : O(N-K+1)
+def averageUsingSlidingWindow(arr, n):
+    res = []
+    aggregate = sum(arr[0:n])
+    res.append(aggregate/n)
+    for i in range(1, len(arr)-n+1):
+        aggregate -= (arr[i-1]-arr[i+n-1])
+        res.append(aggregate/n)
+    return res
+if __name__ == "__main__":
+    print(averageUsingSlidingWindow([1, 3, 2, 6, -1, 4, 1, 8, 2], 5))
